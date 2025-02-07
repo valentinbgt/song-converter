e48df774ed03af397046d81dc31c1704b9a62aea
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-02-07 15:18:34 +0100

    Improved home page
    Added Redirect and NewTab toggles and Submit button

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
     <h1>Paste your link</h1>
-    <UInput v-model="value" />
+    <UInput v-model="link" />
+    <p>
+      <UToggle v-model="redirect" />
+      <span>Auto redirect <span class="text-xs italic">when pasting detected</span></span>
+    </p>
+    <p>
+      <UToggle v-model="newTab" />
+      <span>Open in new tab</span>
+    </p>
+    <UButton :disabled="!isUrlValid(link)">Convert</UButton>
   </div>
 </template>
 
 <script setup lang="ts">
-const value = ref('')
+const link = ref('')
+const redirect = ref(true)
+const newTab = ref(false)
+
+
+function isUrlValid(str: string): boolean {
+  try {
+    new URL(str);
+    return true;
+  } catch {
+    return false;
+  }
+}
 </script>
